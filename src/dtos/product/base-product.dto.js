095dbe3ba0350bfdf3371e28782ddbf3bfd88498
XxSojasxX <sojasvaca@gmail.com>
2023-11-03 18:12:14 -0500

    dtos

---
+++ src/dtos/product/base-product.dto.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BaseProductDto = void 0;
+class BaseProductDto {
+}
+exports.BaseProductDto = BaseProductDto;
+//# sourceMappingURL=base-product.dto.js.map
